1d7aaacab8be36bcb414389d2b1419461883b204
shawn0527 <shawnren527@gmail.com>
2019-10-09 12:50:22 -0500

    create table, insert records

--- test.py
+++ test.py
@@ -2,7 +2,25 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host="127.0.0.1",
-    user="root"
+    user="root",
+    database="testdb"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+# mycursor.execute("SHOW TABLES")
+
+# for table in mycursor:
+#     print(table)
+# mycursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(10))")
+sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+students = [("Chandler", 24),
+            ("Ross", 24),
+            ("Pheobe", 23),
+            ("Joey", 24),
+            ("Mike", 24)]
+
+mycursor.executemany(sqlFormula, students)
+
+mydb.commit()
+
