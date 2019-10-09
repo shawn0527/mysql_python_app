9d0261e572ea4ce3502970de6ca348a272772428
shawn0527 <shawnren527@gmail.com>
2019-10-09 13:25:04 -0500

    select and getting data

--- test.py
+++ test.py
@@ -7,20 +7,45 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-
 # mycursor.execute("SHOW TABLES")
+
+# Tutorial 2:
 
 # for table in mycursor:
 #     print(table)
 # mycursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(10))")
-sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
-students = [("Chandler", 24),
-            ("Ross", 24),
-            ("Pheobe", 23),
-            ("Joey", 24),
-            ("Mike", 24)]
+# sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+# student1 = ("Rachel", 22)
+# mycursor.execute(sqlSormula, student1)
+# mydb.commit()
 
-mycursor.executemany(sqlFormula, students)
+#Tutorial 3:
+# students = [("Chandler", 24),
+#             ("Ross", 24),
+#             ("Pheobe", 23),
+#             ("Joey", 24),
+#             ("Mike", 24)]
+# mycursor.executemany(sqlFormula, students)
+# mydb.commit()
 
-mydb.commit()
+#Tutorial 4:
+# mycursor.execute("SELECT age FROM students")
+# myresult = mycursor.fetchone()
+
+# for row in myresult:
+#     print(row)
+
+#Tutorial 5:
+# sql1 = "SELECT * FROM students WHERE age = 22"
+# sql2 = "SELECT age FROM students WHERE name = 'ROSS'"
+# sql3 = "SELECT * FROM students WHERE name LIKE 'R%'"
+# sql4 = "SELECT * FROM students WHERE name LIKE '%a%'"
+# sql5 = "SELECT * FROM students WHERE name = %s"
+
+# mycursor.execute(sql5, ("Ross",))
+
+# myresults = mycursor.fetchall()
+
+# for result in myresults:
+#     print(result)
 
